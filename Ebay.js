2d6a6439ab55075325ac6979aba26ca469034b76
Chigozie Ugwuede <sparkyglobe@gmail.com>
2021-01-03 15:39:14 +0100

    Updated the response

--- Ebay.js
+++ Ebay.js
@@ -154,14 +154,15 @@ class Ebay {
       {}
     );
 
-    sortedArr.type = `Date and numbers of sold ${this._text} from ebay`;
-
     await this._page.waitForTimeout(3000);
 
     // close browser instance
     await this._browser.close();
 
-    return sortedArr;
+    return {
+      type: `Date and numbers of sold ${this._text} from ebay`,
+      data: sortedArr,
+    };
   };
 }
 
